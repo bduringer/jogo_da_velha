874019713bf7d3dd5ae3c8fcde04c7d1f1104813
bduringer <bduringer@yahoo.com.br>
2021-12-09 17:02:39 -0300

    jogo da velha

---
+++ interface.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded' , () =>
+{
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) =>{
+    square.addEventListener('click', handleClick);
+
+    })
+
+})
+
+function handleClick(event){
+
+   
+    let square = event.target;
+    let position = square.id;
+    if(playerMove(position)){
+       setTimeout(()=>{
+           alert("O jogo acabou");
+       },10);
+        
+    };
+    updatesquares();
+}
+function updatesquares(){
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        let position = square.id;
+        let symbol = board[position];
+
+        if(symbol != ''){
+            square.innerHTML = `<div class = ${symbol}></div>`;
+        }
+
+    })
+}
